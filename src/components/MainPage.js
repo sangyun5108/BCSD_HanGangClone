eb5487b82b61246982296e602a748b010ed426d7
Sangyun <tkfkd4447@naver.com>
2021-08-01 15:19:59 +0900

    Acommodate Code Reveiw in MainPage.js
    이벤트 처리 함수 등록 및 해제를 useEffect를 사용하여 선언해주었습니다.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import MainBigPage from './MainBigPage';
 import MainSmallPage from './MainSmallPage';
 
@@ -6,16 +6,23 @@ const MainPage = () => {
 
     let [isMobile,setIsMobile] = useState(false);
 
-    const checkScreenWidth = () => {
-        let checkScreen = window.matchMedia('(max-width:574px)').matches;
-        if(checkScreen!==isMobile){
-            setIsMobile(checkScreen);
-        }
-    } //화면의 넓이를 확인하는 함수
+    useEffect(()=>{
+        console.log('useEffect');
+        const checkScreenWidth = () => {
+            let checkScreen = window.matchMedia('(max-width:574px)').matches;
+            if(checkScreen!==isMobile){
+                setIsMobile(checkScreen);
+            }
+        } //화면의 넓이를 확인하는 함수
+        window.addEventListener('resize',checkScreenWidth);
+
+        return () => {
+            window.removeEventListener('resize',checkScreenWidth);
+        } //useEffect 실행되기 전에 호출되어 기존에 EventListener를 remove해준다
+    },[isMobile]);
     
     return (
     <>
-        {window.addEventListener('resize',checkScreenWidth)}
         {isMobile?<MainSmallPage/>:<MainBigPage/>}
     </>
     )
