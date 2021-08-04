74b0f985884b2de7dc91e4a4c7331cac7c97a7df
Sangyun <tkfkd4447@naver.com>
2021-08-05 02:54:46 +0900

    Make lists of timetable

--- src/components/MainPageTimeTable.js
+++ src/components/MainPageTimeTable.js
@@ -32,23 +32,60 @@ const TimeTableWrapper = styled.div`
         width:360px;
     }
 `;
-const TimeTableContextWrapper = styled.div`
+
+const TimeTableUl = styled.ul`
+    width:100%;
+    height:304px;
+    padding-left:0px;
+`;
+
+const TimeTableList = styled.li`
+    list-style:none;
+    border-bottom:1px solid rgb(238, 238, 238);
+    padding-left:20px;
+    display:flex;
+    justify-content:space-between;
+`;
+
+const TimeTableListLeftContents = styled.div`
+    height:75px;
+    width:200px;
     display:flex;
     flex-direction:column;
+    justify-content:center;
+`;
+
+const TimeTableListLeftContentTitle = styled.div`
+    font-weight:700;
+`;
+
+const TimeTableListLeftContentProfessor = styled.div`
+    font-size:14px;
+    color:#999999;
+`;
+
+const TimeTableListRightContents = styled.div`
+    width:100px;
+    display:flex;
+    justify-content:center;
     align-items:center;
-    color:grey;
 `;
-const TimeTableMainContext = styled.div`
-`;
-const TimeTableContextButton = styled.button`
-    width:120px;
-    height:30px;
-    margin-top:5px;
-    border-radius:15px;
+
+const TimeTableListRightButton = styled.button`
+    height:25px;
     border:none;
+    border-radius:15px;
+    background-color:${props=>props.active?'':'#238bfe'};
+    color:${props=>props.active?'':'#ffffff'};
     cursor:pointer;
 `;
 
+const TIMETABLE_LIST = [
+    {name:'사랑의 역사',professor:'김사랑',evaluate:'평가완료',rating:3.2,id:'사랑의 역사'},
+    {name:'하트의 역사',professor:'박사랑',evaluate:'평가하기',rating:3.2,id:'하트의 역사'},
+    {name:'사랑의 역사',professor:'김사랑',evaluate:'평가하기',rating:3.2,id:'사랑의 역사2'},
+    {name:'사랑의 역사',professor:'김사랑',evaluate:'평가하기',rating:3.2,id:'사랑의 역사3'}
+]
 
 const MainPageTimeTable = () => {
     return(
@@ -56,10 +93,21 @@ const MainPageTimeTable = () => {
             <TimeTableSectionWrapper>
                 <TimeTableTitleWrapper>내 시간표</TimeTableTitleWrapper>
                 <TimeTableWrapper>
-                    <TimeTableContextWrapper>
-                        <TimeTableMainContext>아직 작성한 시간표가 없습니다.</TimeTableMainContext>
-                        <TimeTableContextButton onClick={()=>alert('작성 불가능합니다')}>작성하러 가기</TimeTableContextButton>
-                    </TimeTableContextWrapper>
+                    <TimeTableUl>
+                        {TIMETABLE_LIST.map((list)=>{
+                            return(
+                                <TimeTableList key={list.id}>
+                                    <TimeTableListLeftContents>
+                                        <TimeTableListLeftContentTitle>{list.name}</TimeTableListLeftContentTitle>
+                                        <TimeTableListLeftContentProfessor>{list.professor}</TimeTableListLeftContentProfessor>
+                                    </TimeTableListLeftContents>
+                                    <TimeTableListRightContents>
+                                        <TimeTableListRightButton active={list.evaluate==='평가완료'}>{list.evaluate}</TimeTableListRightButton>
+                                    </TimeTableListRightContents>
+                                </TimeTableList>
+                            )
+                        })}
+                    </TimeTableUl>
                 </TimeTableWrapper>
             </TimeTableSectionWrapper>
         </>
